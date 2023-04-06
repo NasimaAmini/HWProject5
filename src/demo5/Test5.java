aa2bce8518a96ef9521e08812a363402fba2df55
nasim <nasim@10.0.0.10>
2023-04-06 03:11:13 -0400

    This is Project5

---
+++ src/demo5/Test5.java
@@ -0,0 +1,11 @@
+package demo5;
+
+public class Test5 {
+	public static void main(String[] args) {
+		
+		System.out.println("This is project5");
+		
+		System.out.println("Print project5");
+	}
+
+}
